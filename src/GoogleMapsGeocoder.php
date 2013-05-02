1faaed3aedd76b51435da5672aac04a0f56300cc
Justin Stayton <justin.stayton@gmail.com>
2013-05-02 16:57:24 -0700

    Bump version to 2.0.2.

--- src/GoogleMapsGeocoder.php
+++ src/GoogleMapsGeocoder.php
@@ -8,7 +8,7 @@
    * @license   https://github.com/jstayton/Miner/blob/master/LICENSE-MIT MIT
    * @link      https://developers.google.com/maps/documentation/geocoding/
    * @package   GoogleMapsGeocoder
-   * @version   2.0.1
+   * @version   2.0.2
    */
   class GoogleMapsGeocoder {
 

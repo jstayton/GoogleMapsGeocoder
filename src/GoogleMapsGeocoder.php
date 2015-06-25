760764672ab10b94b2ef5764fcec0c9fb493c344
Justin Stayton <justin.stayton@gmail.com>
2015-06-24 21:18:40 -0400

    Version 2.2.0

--- src/GoogleMapsGeocoder.php
+++ src/GoogleMapsGeocoder.php
@@ -8,7 +8,7 @@
    * @license   https://github.com/jstayton/Miner/blob/master/LICENSE-MIT MIT
    * @link      https://developers.google.com/maps/documentation/geocoding/
    * @package   GoogleMapsGeocoder
-   * @version   2.1.0
+   * @version   2.2.0
    */
   class GoogleMapsGeocoder {
 
